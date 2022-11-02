389e25c401d2a8f56c4c2c0e1a6e449b4dad796c
Ricardo Ortiz <75132128+RicardoOVE@users.noreply.github.com>
2022-11-01 19:38:18 -0500

    Added server & client

---
+++ server.js
@@ -0,0 +1,20 @@
+/*
+npm init -y
+"start": "nodemon server.js"
+npm install express mongoose cors
+create subfolders (server folders)
+
+2nda terminal (instalación React):
+npx create-react-app client
+npm install axios react-router-dom@5.3.0
+
+cd client <- si genera error 
+npm start
+
+1ra terminal:
+npm start
+
+Agregar link a bootstrap en index.html
+Quitar <React.StrictMode> en index.js
+*/
+
