0eea1c2a318a1eaaaeca7a790cb309e5e49d4640
Ricardo Ortiz <75132128+RicardoOVE@users.noreply.github.com>
2022-11-10 20:02:11 -0500

    Added Auth

---
+++ server/config/jwt.config.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+const secret_key = "This is my secret key";
+
+module.exports.authenticate = (req, res, next) => {
+    jwt.verify(req.cookies.usertoken, secret_key, (err, payload) => {
+        if(err) {
+            res.status(401).json({message: "No estás autorizado!"})
+        } else {
+            next();
+        }
+    })
+}
--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const Usuario = require("../models/user.model");
 const jwt = require('jsonwebtoken');
 const secret_key = "This is my secret key"; //Debe de ser la misma a lo largo de toda nuestra app
+const bcrypt = require('bcrypt') //Importación de bcrypt -> npm install bcrypt
 
 module.exports.register = (req, res) => {
     const user = new Usuario(req.body);
@@ -20,4 +21,37 @@ module.exports.register = (req, res) => {
 
         })
         .catch(err => res.status(400).json(err));
+}
+
+module.exports.login = (req, res) => {
+    Usuario.findOne({email: req.body.email})
+        .then(user => {
+            if(user === null) {
+                res.json({error: true, message: "El correo electrónico es incorrecto."});
+            } else {
+                bcrypt.compare(req.body.password, user.password)
+                    .then(passwordValid => {
+                        if(passwordValid) {
+                            const payload = {
+                                _id: user._id
+                            }
+                            const myJWT = jwt.sign(payload, secret_key);
+
+                            res
+                                .cookie("usertoken", myJWT, secret_key, {
+                                    httpOnly: true
+                                })
+                                .json({error: false, message: "Inicio de sesión correcto"})
+
+                        } else {
+                            res.json({error: true, message: "La contraseña es incorrecta."});
+                        }
+                    })
+            }
+        })
+}
+
+module.exports.logout = (req, res) => {
+    res.clearCookie('usertoken');
+    res.json({message: "Salimos de sesión!"});
 }
